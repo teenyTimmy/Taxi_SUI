dbf9e55a3dcd54bfaadc48cc8f85665b62838808
Tim Fedorchenko <fear.7d@gmail.com>
2023-02-25 02:26:06 +0600

    Initial Commit

---
+++ Taxi_SUI/Taxi_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taxi_SUIApp.swift
+//  Taxi_SUI
+//
+//  Created by Tim Fedorchenko on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Taxi_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
